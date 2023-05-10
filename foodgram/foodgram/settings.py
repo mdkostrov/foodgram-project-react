23ffee266c6a2a2a66417f5caf20048f1e8f237f
Михаил Костров <mdkostrov@gmail.com>
2023-05-11 01:12:00 +0300

    prod

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default='DefaultSecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ENABLE_PROD = False
+ENABLE_PROD = True
 
 DEBUG = True
 
